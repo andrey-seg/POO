79503757da1e80a351704f7304d8d770f00c62c6
Andrey Segobia <andrey.segobia.prol@gmail.com>
2026-08-12 16:53:17 -0400

    adding the exercice final part

--- Exer015/src/services/AcademicServices.ts
+++ Exer015/src/services/AcademicServices.ts
@@ -37,6 +37,54 @@ export class AcademicService{
     };
 
     enrollStudent(studentID: string, subjectID: string): I_ApiResponse<Student>{
+        const studentExist = this.__StudentList.find(s => s.getId() === studentID);
+        const subjectExist = this.__SubjectList.find(s => s.getId() === subjectID);
 
-    }
+        if(!studentExist || !subjectExist){
+            return { success: false, error: `Subject or student not found`};
+        };
+
+        try{
+            studentExist.enroll(subjectExist);
+            return { success: true, data: studentExist};
+        }catch(error){
+            return { success:false, error: (error as Error).message };
+        };
+    };
+
+    submitGrade(studentID: string, subjectID: string, value: number): I_ApiResponse<Student>{
+        const studentExist = this.__StudentList.find(s => s.getId() === studentID);
+        const subjectExist = this.__SubjectList.find(s => s.getId() === subjectID);
+
+        if(!studentExist || !subjectExist){
+            return { success: false, error: `Subject or student not found`};
+        };
+
+        try{
+            studentExist.addGrade(subjectID, value);
+            return { success: true, data: studentExist};
+        }catch(error){
+            return { success: false, error: (error as Error).message};
+        };
+    };
+
+    getStudentReport(studentID: string): I_ApiResponse<Student>{
+        const studentExist = this.__StudentList.find(s => s.getId() === studentID);
+
+        if(!studentExist){
+            return { success: false, error: `Student not found`};
+        };
+
+        return {success: true, data: studentExist};
+    };
+
+    getPassingStudents(): I_ApiResponse<Student[]>{
+        const passingStudents = this.__StudentList.filter(s => s.getGPA() >= 6);
+
+        if(passingStudents.length === 0){
+            return { success: false, error: `No passing students found`};
+        };
+
+        return { success: true, data: passingStudents };
+    };
 }
