3bd8d9095b980c805b29eeb9716c17be06fe2683
Andrey-seg <andrey.segobia.pro@gmail.com>
2026-09-05 18:18:18 -0400

    adding User and Product to project

--- Exer020/src/models/Product.ts
+++ Exer020/src/models/Product.ts
@@ -1,3 +1,98 @@
+import { ProductStatus } from "../enums/ProductStatus";
+
 export class Product{
     
+    private __id: string;
+    private __name: string;
+    private __price: number;
+    private __stock: number;
+    private __status: ProductStatus;
+
+    constructor(id: string, name: string, price: number, stock: number, status: ProductStatus){
+
+        this.__id = id;
+        this.__name = name;
+        this.__price = price;
+        this.__stock = stock;
+        this.__status = status;
+    }
+
+    deacreseStock(quantity: number): number{
+
+        if(this.__stock < quantity){
+            throw new Error(`Stock on product is 0`);
+        }
+
+        return this.__stock -= quantity;
+    }
+
+    increaseStock(quantity: number): number{
+        return this.__stock += quantity;
+    }
+
+    isAvailable(quantity: number): boolean{
+        return this.__stock >= quantity;
+    }
+
+    activate(): boolean{
+
+        if(this.__status === ProductStatus.OUT_OF_STOCK){
+            throw new Error(`Product out of stock`);
+        }
+        
+        if(this.__status === ProductStatus.ACTIVE){
+            throw new Error(`Status is alredy active`);
+        }
+
+        this.__status = ProductStatus.ACTIVE;
+        return true;
+    }
+
+    deactivate(): boolean{
+
+        if(this.__status === ProductStatus.OUT_OF_STOCK){
+            throw new Error(`Product out of stock`);
+        }
+
+        if(this.__status === ProductStatus.INACTIVE){
+            throw new Error(`Product alredy inactive`);
+        }
+
+        this.__status = ProductStatus.INACTIVE;
+        return true;
+    }
+
+    marlOutOfStock(): boolean{
+
+        if(this.__status === ProductStatus.OUT_OF_STOCK){
+            throw new Error(`Product alredy out stock`);
+        }
+
+        this.__status = ProductStatus.OUT_OF_STOCK;
+        return true;
+    }
+
+    toString(): string{
+        return `Id => ${this.__id} | Name => ${this.__name} | Price => ${this.__price} | Stock => ${this.__stock} | Status => ${this.__status}`;
+    }
+
+    getId(): string{
+        return this.__id;
+    }
+
+    getName(): string{
+        return this.__name;
+    }
+
+    getPrice(): number{
+        return this.__price;
+    }
+
+    getStock(): number{
+        return this.__stock;
+    }
+
+    getStatus(): ProductStatus{
+        return this.__status;
+    }
 }
--- Exer020/src/models/User.ts
+++ Exer020/src/models/User.ts
@@ -1,3 +1,57 @@
+import { UserRole } from "../enums/UserRole"
+
 export class User{
     
+    private __id: string;
+    private __name: string;
+    private __email: string;
+    private __password: string;
+    private __role: UserRole;
+    private __createdAt: string;
+
+    constructor(id: string, name: string, email: string, password: string, role: UserRole){
+
+        this.__id = id;
+        this.__name = name;
+        this.__email = email;
+        this.__password = password;
+        this.__role = role;
+        this.__createdAt = new Date().toISOString();
+    }
+
+    validatePassword(password: string): boolean{
+        return this.__password === password;
+    }
+
+    isAdmin(): boolean{
+        return this.__role === UserRole.ADMIN;
+    }
+
+    toString(): string{
+        return `Id => ${this.__id} | Name => ${this.__name} | Email => ${this.__email} | Password => Cannot get | Role => ${this.__role} | Created At => ${this.__createdAt}`;
+    }
+
+    getId(): string{
+        return this.__id;
+    }
+
+    getName(): string{
+        return this.__name;
+    }
+
+    getEmail(): string{
+        return this.__email;
+    }
+
+    getPassword(): string{
+        return this.__password;
+    }
+
+    getRole(): UserRole{
+        return this.__role;
+    }
+
+    getCreatedAt(): string{
+        return this.__createdAt;
+    }
 }
