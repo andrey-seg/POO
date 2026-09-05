96be1aa455c31424666ff01d9b12309f33f1561d
Andrey-seg <andrey.segobia.pro@gmail.com>
2026-09-05 18:25:58 -0400

    adding CartItem to project

--- Exer020/src/models/CartItem.ts
+++ Exer020/src/models/CartItem.ts
@@ -1,3 +1,43 @@
+import { Product } from "./Product";
+
 export class CartItem{
     
+    private __id: string;
+    private __product: Product;
+    private __quantity: number;
+
+    constructor(id: string, product: Product, quantity: number){
+
+        this.__id = id;
+        this.__product = product;
+        this.__quantity = quantity;
+    }
+
+    subTotal(): number{
+        return this.__product.getPrice() * this.__quantity;
+    }
+
+    increaseQuantity(quantity: number): number{
+        return this.__quantity += quantity;
+    }
+
+    decreaseQuantity(quantity: number): number{
+        return this.__quantity -= quantity;
+    }
+
+    toString(): string{
+        return `Id => ${this.__id} | Product => ${this.__product} | Quantity => ${this.__quantity}`;
+    }
+
+    getId(): string{
+        return this.__id;
+    }
+
+    getProduct(): Product{
+        return this.__product;
+    }
+
+    getQuantity(): number{
+        return this.__quantity;
+    }
 }
