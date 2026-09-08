5dd879c26699bedecf8e8e1f6abe08645d288181
Andrey-seg <andrey.segobia.pro@gmail.com>
2026-09-08 15:04:41 -0400

    adding UserRepository to the project

--- Exer020/src/interfaces/I_Repository.ts
+++ Exer020/src/interfaces/I_Repository.ts
@@ -1,6 +1,6 @@
 export interface I_Repository<T>{
     findById(id: string): Promise<T | null>;
-    findAll(): Promise<T>;
-    save(entity: T): Promise<T[]>;
+    findAll(): Promise<T[]>;
+    save(entity: T): Promise<T>;
     delete(id: string): Promise<boolean>;
 }
--- Exer020/src/repositories/UserRepository.ts
+++ Exer020/src/repositories/UserRepository.ts
@@ -3,4 +3,56 @@ import { User } from "../models/User";
 
 export class UserRepository implements I_Repository<User>{
 
+    private __userList: User[] = [];
+
+    findById(id: string): Promise<User | null> {
+        
+        return new Promise((resolve) => {
+
+            const findUserById = this.__userList.find((u) => u.getId() === id);
+            resolve(findUserById ?? null);
+        });
+    }
+
+    findAll(): Promise<User[]> {
+        
+        return new Promise((resolve) => {
+            resolve(this.__userList);
+        });
+    }
+
+    save(entity: User): Promise<User> {
+        
+        return new Promise((resolve) => {
+            this.__userList.push(entity);
+            resolve(entity);
+            return;
+        });
+    }
+
+    delete(id: string): Promise<boolean> {
+        
+        return new Promise((resolve) => {
+            const findUserById = this.__userList.findIndex((u) => u.getId() === id);
+
+            if(findUserById === -1){
+                throw new Error(`User not found.`);
+                resolve(false);
+                return;
+            }
+            
+            this.__userList.splice(findUserById, 1);
+            resolve(true);
+            return;
+        });
+    }
+
+    findByEmail(email: string): Promise< User | null >{
+
+        return new Promise((resolve) => {
+
+            const findByEmail = this.__userList.find((u) => u.getEmail() === email);
+            resolve( findByEmail ?? null );
+        });
+    }
 }
