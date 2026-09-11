2ef39bab45f2180938438e2b67ad9bb6c80e4faf
Andrey-seg <andrey.segobia.pro@gmail.com>
2026-09-11 15:31:10 -0400

    adding CartService to project

--- Exer020/src/services/CartService.ts
+++ Exer020/src/services/CartService.ts
@@ -5,13 +5,92 @@ import { Cart } from '../models/Cart';
 
 export class CartService{
     
+    private __carts: Map<string, Cart> = new Map();
+
     constructor(private __productRepository: ProductRepository){};
 
-    getCart(userId: string): I_ApiResponse<Cart>{
+    async getCart(userId: string): Promise<I_ApiResponse<Cart>>{
         
         try{
 
-            const findCart = await this.__productRepository.findById()
+            const cart = this.__carts.get(userId);
+
+            if(!cart){
+                return { success: false, error: `Cart not found.` };
+            }
+
+            return { success: true, data: cart };
+        }catch(error){
+            return { success: false, error: (error as Error).message };
+        }
+    }
+
+    async addToCart(userID: string, productID: string, quantity: number): Promise<I_ApiResponse<Cart>>{
+
+        try{
+
+            const product = await this.__productRepository.findById(productID);
+
+            if(!product){
+                return { success: false, error: `Product not found.` };
+            }
+
+            if(!product.isAvailable(quantity)){
+                return { success: false, error: `Product out of stock.` };
+            }
+
+            let cart = this.__carts.get(userID);
+
+            if(!cart){
+                cart = new Cart(userID);
+                this.__carts.set(userID, cart);
+            }
+
+            cart.addItem(product, quantity);
+            return { success: true, data: cart };
+
+        }catch(error){
+            return { success: false, error: (error as Error).message };
+        }
+    }
+
+    async removeFromCart(userID: string, productID: string): Promise<I_ApiResponse<Cart>>{
+
+        try{
+
+            const findUserById = this.__carts.get(userID);
+            const findProductById = await this.__productRepository.findById(productID);
+
+            if(!findUserById){
+                return { success: false, error: `User not found`}
+            }
+
+            if(!findProductById){
+                return { success: false, error: `Product not found.` };
+            }
+
+            const removed = findUserById.removeItem(productID);
+
+            return{ success: true, data: findUserById };
+        }catch(error){
+            return { success: false, error: (error as Error).message };
+        }
+    }
+
+    async checkout(userID: string): Promise<I_ApiResponse<Cart>>{
+
+        try{
+
+            const findUser = this.__carts.get(userID);
+
+            if(findUser?.isEmpty()){
+                return { success: false, error: `Cart is empty.` };
+            }
+
+            findUser?.clear();
+            return { success: true, data: findUser };
+        }catch(error){
+            return { success: false, error: (error as Error).message };
         }
     }
 }
